a5dc99f694cb786f8886ce694b28f7680040e911
HAMH96 <hmoraleshoyos@gmail.com>
2023-06-04 22:22:57 -0500

    First Commit, task without styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import Profile from "./task1/components/Profile"
+import userData from "./task1/utils/user.json";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
     </div>
   );
 };
